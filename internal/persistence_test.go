4a6bf96732ee14c996dc9cf472f9477dbdf7fa68
Michael Dorner <mail@michaeldorner.de>
2018-12-21 16:12:17 +0100

    fixed import

--- internal/persistence_test.go
+++ internal/persistence_test.go
@@ -3,14 +3,5 @@ package internal
 import "testing"
 
 func TestLoadConfigurationFile(t *testing.T) {
-	var configuration Configuration
-	jsonData, err := ioutil.ReadFile(configurationFilePath)
-	if err != nil {
-		return nil, err
-	}
-
-	if err := json.Unmarshal(jsonData, &configuration); err != nil {
-		return nil, err
-	}
-	return &configuration, nil
+	
 }
