47d5226e4182ffa02fa0383eb8640440a7df4b9c
Michael Dorner <mail@michaeldorner.de>
2019-01-21 20:18:52 +0100

    added test case

--- crawl/item_test.go
+++ crawl/item_test.go
@@ -1,1 +1,15 @@
 package crawl
+
+import "testing"
+
+func TestFileName(t *testing.T) {
+	item := Item{
+		ID: "0001",
+		URL: "https://www.google.com",
+		Payload: []byte("bytes"),
+		FileNameExtensions: ".json",
+	}
+	if item.FileName() != "0001.json" {
+		t.Errorf("Expected %v, got %v\n", "0001.json", item.FileName())
+	}
+}
