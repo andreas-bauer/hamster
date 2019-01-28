9f82e9c0631a5fc225decb67e69412e09c228920
Michael Dorner <mail@michaeldorner.de>
2019-01-28 10:52:37 +0100

    added test cases

--- store/store_test.go
+++ store/store_test.go
@@ -1,6 +1,8 @@
 package store
 
 import (
+	"bytes"
+	"io/ioutil"
 	"os"
 	"path/filepath"
 	"testing"
@@ -43,5 +45,35 @@ func TestLogFile(t *testing.T) {
 
 	if _, err := os.Stat(repository.logFilePath()); os.IsNotExist(err) {
 		t.Errorf("failed in creating log file at %v\n", repository.logFilePath())
+	}
+}
+
+func TestStore(t *testing.T) {
+	path := filepath.Clean("./repository")
+	repository := NewRepository(path)
+
+	b := []byte{0, 1, 2, 3, 4}
+	writeErr := repository.Store(repository.AppendDataPath("test"), b)
+	rb, readErr := ioutil.ReadFile(repository.AppendDataPath("test"))
+
+	if writeErr != nil || readErr != nil {
+		t.Error("failed in writing bytes to file")
+	}
+
+	if !bytes.Equal(b, rb) {
+		t.Errorf("Expected %v, got %v byte array\n", b, rb)
+	}
+}
+
+func TestFielExists(t *testing.T) {
+	path := filepath.Clean("./repository")
+	repository := NewRepository(path)
+	repository.logFilePath()
+	testFilePath := repository.AppendDataPath("test")
+
+	writeErr := repository.Store(testFilePath, []byte{0, 1, 2, 3, 4})
+
+	if writeErr != nil || repository.FileExists(testFilePath) != true {
+		t.Error("method FileExists(...) failed")
 	}
 }
