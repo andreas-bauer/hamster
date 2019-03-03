3968c6cb3d08f829ab8281f162ff56c6c805f1a8
Michael Dorner <mail@michaeldorner.de>
2019-03-03 16:00:17 +0100

    removed untested convenience creation method

--- crawl/item.go
+++ crawl/item.go
@@ -11,18 +11,6 @@ type Item struct {
 	FileNameExtensions string
 }
 
-func NewItem(id, url, FileNameExtensions string) (*Item, error) {
-	request, err := http.NewGetRequest(url)
-	if err != nil {
-		return nil, err
-	}
-	return &Item{
-		ID:                 id,
-		Request:            request,
-		FileNameExtensions: FileNameExtensions,
-	}, nil
-}
-
 func (item Item) FileName() string {
 	return item.ID + "." + item.FileNameExtensions
 }
